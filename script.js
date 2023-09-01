2234e91b67e887b978360060ebd153de94379caa
AnishDhomase <anishdhomase@gmail.com>
2023-09-01 16:20:53 +0530

    preloader added , rocket removed

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 
+const preloader = document.querySelector("#preloader");
 const menuBtn = document.querySelector("nav .navBtnDiv i");
 const wrapper = document.querySelector("#wrapper");
 const modal = document.querySelector("#modal");
 const modalClose = document.querySelector("#modal i");
 const modalNavs = document.querySelectorAll("#modal div a");
+
+window.addEventListener("load",function(){
+    preloader.classList.add("remove");
+    wrapper.classList.remove("remove");
+    
+});
+
 
 menuBtn.addEventListener("click",function(){
     wrapper.classList.add("remove");
